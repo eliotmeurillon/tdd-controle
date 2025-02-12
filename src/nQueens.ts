a33b8c89b084e075dbefca75594b2c2a7f95f816
eliotmeurillon <eliot.meurillon@gmail.com>
2025-02-12 15:36:14 +0100

    implement isSafe function to check queen placement validity

--- src/nQueens.ts
+++ src/nQueens.ts
@@ -11,6 +11,23 @@ export function isSafe(
   col: number,
   n: number
 ): boolean {
-  if (row === 0) return true;
-  return false;
+  for (let i = 0; i < row; i++) {
+    if (board[i][col] === "#") {
+      return false;
+    }
+  }
+
+  for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+    if (board[i][j] === "#") {
+      return false;
+    }
+  }
+
+  for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+    if (board[i][j] === "#") {
+      return false;
+    }
+  }
+
+  return true;
 }
