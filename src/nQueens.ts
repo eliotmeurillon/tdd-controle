f04755a938e95303fea2fbb1b5d7265bf4525ffd
eliotmeurillon <eliot.meurillon@gmail.com>
2025-02-12 15:44:44 +0100

    add test case to validate solveNQueens function for n=3 (no solution)

--- src/nQueens.ts
+++ src/nQueens.ts
@@ -2,7 +2,7 @@ export function solveNQueens(n: number): string[][] {
   if (n === 1) {
     return [["#"]];
   }
-  if (n === 2) {
+  if (n === 2 || n === 3) {
     return [];
   }
   return [];
--- tests/nQueens.test.ts
+++ tests/nQueens.test.ts
@@ -11,6 +11,11 @@ describe("solveNQueens()", () => {
     const solutions = solveNQueens(2);
     expect(solutions).toHaveLength(0);
   });
+
+  test("Devrait retourner un tableau vide pour n=3 (pas de solution)", () => {
+    const solutions = solveNQueens(3);
+    expect(solutions).toHaveLength(0);
+  });
 });
 
 describe("isSafe()", () => {
